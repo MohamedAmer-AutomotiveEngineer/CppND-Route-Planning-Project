55df54a5b86ba6a2dd78ee3855bae90ae1c4293d
MohamedAmer-AutomotiveEngineer <MohamedAmer-AutomotiveEngineer@users.noreply.github.com>
2020-12-20 01:38:52 +0200

    1st TODO
    Completing the first TODO in main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "route_model.h"
 #include "render.h"
 #include "route_planner.h"
-
+using namespace std;
 using namespace std::experimental;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
@@ -25,6 +25,26 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     if( contents.empty() )
         return std::nullopt;
     return std::move(contents);
+}
+
+float getFloat(string name) 
+{
+    char input [20];
+    float ret_val = -0.1f;  // start with invalid value
+  
+    while (true)
+    {
+        cout << "Please enter " << name << ": ";
+        cin.getline(input, 20);
+        ret_val = (float)atof(input);
+        if (ret_val < 0.0f || ret_val > 100.0f) {
+            cout << "Please enter a number between 0-100.\n";
+        } 
+        else 
+        {
+            return ret_val;
+        }
+    }
 }
 
 int main(int argc, const char **argv)
@@ -55,12 +75,16 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
-
+    float start_x, start_y, end_x, end_y;
+    start_x = getFloat("start_x");
+    start_y = getFloat("start_y");
+    end_x   = getFloat("end_x");
+    end_y   = getFloat("end_y");
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
